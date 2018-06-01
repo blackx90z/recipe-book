53e0595835e032885934827ec3e860bb98177dcc
blackx90z <ianmorin23@gmail.com>
2018-06-01 16:23:08 -0700

    creates all required components for the application

---
+++ projects-for-class/src/app/recepies/recepies-list/recepie-item/recepie-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recepie-item',
+  templateUrl: './recepie-item.component.html',
+  styleUrls: ['./recepie-item.component.css']
+})
+export class RecepieItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
