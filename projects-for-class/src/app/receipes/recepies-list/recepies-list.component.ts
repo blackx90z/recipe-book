5b8997322c7d6ad22271831cddca3df50acfef72
blackx90z <ianmorin23@gmail.com>
2018-06-04 16:12:28 -0700

    added Recipe items

--- projects-for-class/src/app/receipes/recepies-list/recepies-list.component.ts
+++ projects-for-class/src/app/receipes/recepies-list/recepies-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes/recipe.model';
 
 @Component({
   selector: 'recepies-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepies-list.component.css']
 })
 export class RecepiesListComponent implements OnInit {
+  recipes: Recipe[] = [{
+    name: ' A new recipe',
+    description: 'test recipe',
+    imagePath:'https://www.inspiredtaste.net/wp-content/uploads/2012/02/Easy-Chicken-Ragu-1200.jpghttps://www.inspiredtaste.net/wp-content/uploads/2012/02/Easy-Chicken-Ragu-1200.jpg',
+
+  }]; new Recipe('A test Recipe',
+   'This is a Simple test.',
+   'http://www.cbc.ca/food/content/images/recipes/ChickenandWaffles.jpg')
 
   constructor() { }
 
